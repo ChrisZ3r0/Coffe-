a5fb129c5e65ae891837a76827bcc5698b05a0a6
ChrisZ3r0 <78749793+ChrisZ3r0@users.noreply.github.com>
2023-09-07 18:30:59 +0200

    Update buttons.py

--- src/features/buttons.py
+++ src/features/buttons.py
@@ -1,7 +1,8 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
-def button_callback(channel):
+def button_callback(channel,pin):
     print("Button was pushed!")
+    print("pin number   " + pin)
 
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
@@ -11,11 +12,11 @@ GPIO.setup(37, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input 
 GPIO.setup(36, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 GPIO.setup(35, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
-GPIO.add_event_detect(35,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
-GPIO.add_event_detect(36,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
-GPIO.add_event_detect(37,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
-GPIO.add_event_detect(38,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
-GPIO.add_event_detect(40,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
+GPIO.add_event_detect(35,GPIO.RISING,callback=button_callback(35)) # Setup event on pin 10 rising edge
+GPIO.add_event_detect(36,GPIO.RISING,callback=button_callback(36)) # Setup event on pin 10 rising edge
+GPIO.add_event_detect(37,GPIO.RISING,callback=button_callback(37)) # Setup event on pin 10 rising edge
+GPIO.add_event_detect(38,GPIO.RISING,callback=button_callback(38)) # Setup event on pin 10 rising edge
+GPIO.add_event_detect(40,GPIO.RISING,callback=button_callback(40)) # Setup event on pin 10 rising edge
 
 message = input("Press enter to quit\n\n") # Run until someone presses enter
 
